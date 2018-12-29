2a64a7fa030ac1d529b20c20e1d9dad64c1000e3
Aravind Merugu <aravind9643@gmail.com>
2018-12-29 10:51:41 +0530

    modified app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,71 @@
 import { Component } from '@angular/core';
+import { stringify } from 'querystring';
+// import { ObjModel } from './model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+  
+
 export class AppComponent {
+
+  ngOnInit() {
+
+    this.loadData();
+  }
+  loadData() {
+    var outarr = [];
+    var data = {
+      "id": {
+        "required": true,
+        "type": "string"
+      },
+      "name": "Aravind"
+    }
+    for (var key in data) {
+      var chobj = data[key];
+      var obj: ObjModel = {
+        innerObj: {
+          value: ""
+        },
+        name: "",
+        showInnerParams: false,
+        type: "",
+        value: ""
+      };
+      obj.type = typeof (chobj);
+      obj.name = key;
+      obj.showInnerParams = false;
+      obj.innerObj.value = "string"
+      if (obj.type == "object") {
+        obj.value = [];
+        for (var chkey in chobj) {
+          let valobj = {
+            "type": typeof (chkey),
+            "value": chobj[chkey],
+            "name": chkey
+          }
+          obj.value.push(valobj);
+        }
+      }
+      else {
+        obj.value = chobj;
+      }
+      outarr.push(obj);
+    }
+    console.log(outarr);
+  }
+}
+
+class ObjModel {
+  type: any;
+  name: string;
+  showInnerParams: boolean;
+  innerObj: InnerObj;
+  value: any;
+}
+class InnerObj {
+  value: string;
 }
---
+++ src/app/model.ts
@@ -0,0 +1,10 @@
+export class ObjModel {
+    type: any;
+    name: string;
+    showInnerParams: boolean;
+    innerObj: InnerObj;
+    value: any;
+  }
+  export class InnerObj {
+    value: string;
+  }
